4bc33d8bda736b05db105b2b3ba3ba55f3f2f4ce
theodickson <theo@Theodores-MBP.home>
2017-09-15 20:39:30 +0100

    user-friendly access to underlying table objects
    useful for SQLAlchemy Core API

---
+++ pyphonograph/orm/tables.py
@@ -0,0 +1,7 @@
+from . import Artist,Album,Track,ProcessedArtist,ProcessedTrack
+
+artist_table = Artist.__table__
+album_table = Album.__table__
+track_table = Track.__table__
+processed_artist_table = ProcessedArtist.__table__
+processed_track_table = ProcessedTrack.__table__
