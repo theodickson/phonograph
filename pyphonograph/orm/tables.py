cb4a2a0380d9689ac13ec84fad093d94989b0cd5
theodickson <theo@Theodores-MBP.home>
2017-09-16 19:43:33 +0100

    add group_table

--- pyphonograph/orm/tables.py
+++ pyphonograph/orm/tables.py
@@ -1,6 +1,7 @@
-from . import Artist,Album,Track,ProcessedArtist,ProcessedTrack
+from . import Artist,Album,Track,Group,ProcessedArtist,ProcessedTrack
 
 artist_table = Artist.__table__
+group_table = Group.__table__
 album_table = Album.__table__
 track_table = Track.__table__
 processed_artist_table = ProcessedArtist.__table__
