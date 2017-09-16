c6d5a371e405fe19848e297d4dc8e9e6b4c29394
theodickson <theo@Theodores-MBP.home>
2017-09-16 19:42:20 +0100

    remove attempts at indexing groups via ORM
    its now done in raw SQL

--- pyphonograph/orm/__main__.py
+++ pyphonograph/orm/__main__.py
@@ -8,19 +8,35 @@ from .. import utils
 
 engine = get_engine()
 new_session = get_new_session()
+new_connection = get_new_connection()
 
 logging.basicConfig()
 # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-def add_missing_groups():
-    with new_session(autoflush=False) as session:
-        tracks = session.query(Track).filter(Track.group == None).limit(100)
-        c = 0
-        for track in tqdm(tracks):
-            group = Group.from_artists(track.artists, add_artists=False)
-            assert group not in session
-            track.artists[0].groups.append(group)
-            assert group not in session
+# def add_missing_groups_core():
+#     #1. first we need to add all artist groups to the database 
+#     with new_connection() as conn:
+        
+# def add_missing_groups():
+#     #. 
+#     with new_session(autoflush=False) as session:
+#         tracks = session.query(Track).filter(Track.group == None).limit(100)
+#         c = 0
+#         for track in tqdm(tracks):
+#             group = Group.from_artists(track.artists, add_artists=True)
+#             session.merge(group)
+#             new_track = Track(id_=track.id_, album_id=track.album_id, name=track.name, group_id=group.id_, duration_ms=track.duration_ms, external_urls=track.external_urls)
+#             session.merge(new_track
+
+            #adding the artists to the group is adding the group to the session (therefore new groups are being added not merged)
+            #need to either figure out cascades (being careful not to break the way crawling adds everything)
+            #or find a way to add the artists to the group without triggering this behaviour
+            #the key may be that the normal way doesn't add 'live' artists to the group
+
+            #this was true, I can create the group from new copies of the artists (from_artists2) and merge the whole thing
+            #however I now have the same problem with the track. and the same fix doesnt work. there's something much deeper here
+            #i'm not understanding...
+
             #assert group not in session
             #session.merge(group)
             #group.artists = track.artists
