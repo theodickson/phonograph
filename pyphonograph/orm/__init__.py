5f7d80428a4570bfd16168887d3935c9e775d415
theodickson <theo@Theodores-MBP.home>
2017-09-17 17:31:49 +0100

    update imports

--- pyphonograph/orm/__init__.py
+++ pyphonograph/orm/__init__.py
@@ -1,4 +1,5 @@
-from .raw import Artist,Group,Album,Track
+from .raw import Artist,Group,Album,Track,EntityRemovalInfo
 from .processed import ProcessedArtist,ProcessedTrack
 from .utils import *
 from .base import Base
+from .views import views
